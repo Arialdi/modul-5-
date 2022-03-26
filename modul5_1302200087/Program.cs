9a06656af727576eb742dd1ce0fe5487c48e92b9
Arialdi <arialdirahmadiputra@student.telkomuniversity.ac.id>
2022-03-26 16:40:12 +0700

    Menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul5_1302200087/Program.cs
+++ modul5_1302200087/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul5_1302200087
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo j1 = new SayaTubeVideo("Review Film Batman oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j2 = new SayaTubeVideo("Review Film The Red oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j3 = new SayaTubeVideo("Review Film Sing 2 oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j4 = new SayaTubeVideo("Review Film Jungle Cruise oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j5 = new SayaTubeVideo("Review Film Free Guy oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j6 = new SayaTubeVideo("Review Film Kurang Kejaan oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j7 = new SayaTubeVideo("Review Film Gak tau Apa oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j8 = new SayaTubeVideo("Review Film Lupa Lagi oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j9 = new SayaTubeVideo("Review Film Bingung oleh Arialdi Rahmadi Putra");
+            SayaTubeVideo j10 = new SayaTubeVideo("Review Film Mau Ngapain oleh Arialdi Rahmadi Putra");
+            SayaTubeUser ss = new SayaTubeUser("Arialdi Rahmadi Putra");
+
+            j1.IncreasePlayCount(1);
+            j2.IncreasePlayCount(5);
+            j3.IncreasePlayCount(3);
+            j4.IncreasePlayCount(2);
+            j5.IncreasePlayCount(4);
+            j6.IncreasePlayCount(5);
+            j7.IncreasePlayCount(8);
+            j8.IncreasePlayCount(5);
+            j9.IncreasePlayCount(4);
+            j10.IncreasePlayCount(2);
+
+
+            ss.AddVideo(j1);
+            ss.AddVideo(j2);
+            ss.AddVideo(j3);
+            ss.AddVideo(j4);
+            ss.AddVideo(j5);
+            ss.AddVideo(j6);
+            ss.AddVideo(j7);
+            ss.AddVideo(j8);
+            ss.AddVideo(j9);
+            ss.AddVideo(j10);
+
+            j1.PrintVideoDetails();
+
+            ss.PrintAllVideoPlaycount();
+        }
+    }
+}
---
+++ modul5_1302200087/SayaTubeVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302200087
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int a)
+        {
+            this.playCount += a;
+        }
+        public int getPlayCount()
+        {
+            return this.playCount;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+        public void PrintVideoDetails()
+        {
+            for (int i = 0; i < playCount; i++)
+            {
+
+                Console.WriteLine("ID Film : " + this.id);
+                Console.WriteLine("Title Film : " + this.title);
+                Console.WriteLine("Playcount Film : " + (i + 1));
+                Console.WriteLine();
+            }
+        }
+    }
+}
